7946c108e3b775f06fd289bb4ddc96ee6a77887a
zxmacc <71143103+zxmacc@users.noreply.github.com>
2022-06-09 15:10:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="uptime-v1.fly.dev";
+        url.hostname="uptime-v2.fly.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
